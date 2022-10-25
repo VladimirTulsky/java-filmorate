d31ca271dd20656f6243ad7925d48e1a07dc9802
Vladimir <vladimit.tulskiy@yandex.ru>
2022-10-25 20:52:11 +0300

    feat : add classes

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.model;
+
+import java.time.LocalDate;
+
+public class Film {
+
+    private int id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
+
+}
