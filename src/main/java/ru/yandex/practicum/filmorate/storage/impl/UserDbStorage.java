4639c442ef85901f5358ba9e3a7bc0d20cb4cf1b
Vladimir <vladimit.tulskiy@yandex.ru>
2022-12-19 23:40:50 +0300

    1.17 feat : delete films, users

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/UserDbStorage.java
@@ -75,9 +75,11 @@ public class UserDbStorage implements UserStorage {
     @Override
     public Optional<User> deleteById(int id) {
         String friendshipSql = "DELETE FROM friendship WHERE user_id OR friend_id = ?";
-        String sql = "delete from USERS where USER_ID = ?";
+        String likesSql = "DELETE FROM films_likes WHERE user_id = ?";
+        String sql = "DELETE FROM users WHERE user_id = ?";
         Optional<User> user = getById(id);
         jdbcTemplate.update(friendshipSql, id);
+        jdbcTemplate.update(likesSql, id);
         jdbcTemplate.update(sql, id);
 
         return user;
