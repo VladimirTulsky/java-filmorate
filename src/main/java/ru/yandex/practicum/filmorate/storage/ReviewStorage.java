a34c076c06fffb8e4e6144180e5e0fd0d35e61dd
Anna <annavvoronina@yandex.ru>
2022-12-25 19:20:33 +0300

    Added review module

--- src/main/java/ru/yandex/practicum/filmorate/storage/ReviewStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/ReviewStorage.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ReviewStorage {
+
     Review create(Review review);
     Optional<Review> update(Review review);
     Optional<Review> deleteById(int id);
